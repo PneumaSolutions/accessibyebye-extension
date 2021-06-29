ae2739631860e48a3302a41b038468ea98510fa2
David Calvo <dcalvo2@jhu.edu>
2021-06-28 20:39:56 -0400

    ci: Update build config

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,35 +1,35 @@
-const webpack = require("webpack");
-const path = require("path");
-const srcDir = path.join(__dirname, "..", "src/");
-const CopyPlugin = require("copy-webpack-plugin");
+const webpack = require("webpack")
+const path = require("path")
+const srcDir = path.join(__dirname, "..", "src/")
+const CopyPlugin = require("copy-webpack-plugin")
 
 module.exports = {
-    entry: {
-        popup: path.join(srcDir + 'popup.tsx'),
-        background: path.join(srcDir + 'background.ts'),
-        options: path.join(srcDir + 'options.tsx'),
-        hot_reload: path.join(srcDir + 'hot_reload.ts'),
-    },
-    output: {
-        path: path.join(__dirname, "../dist/js"),
-        filename: "[name].js",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: [".ts", ".tsx", ".js"],
-    },
-    plugins: [
-        new CopyPlugin({
-            patterns: [{ from: ".", to: "../", context: "public" }],
-            options: {},
-        }),
+  entry: {
+    popup: path.join(srcDir + "popup.tsx"),
+    background: path.join(srcDir + "background.ts"),
+    options: path.join(srcDir + "options.tsx"),
+    hot_reload: path.join(srcDir + "hot_reload.ts"),
+  },
+  output: {
+    path: path.join(__dirname, "../dist/js"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
     ],
-};
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: ".", to: "../", context: "public" }],
+      options: {},
+    }),
+  ],
+}
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,6 +1,35 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const merge = require("webpack-merge")
+const common = require("./webpack.common.js")
+const CopyPlugin = require("copy-webpack-plugin")
+
+// Overwrite manifest.json with production settings
+function modify(buffer) {
+  // copy-webpack-plugin passes a buffer
+  var manifest = JSON.parse(buffer.toString())
+
+  // remove hot_reload.js
+  manifest.background.scripts = manifest.background.scripts.slice(0, 2)
+
+  // pretty print to JSON with two spaces
+  manifest_JSON = JSON.stringify(manifest, null, 2)
+  return manifest_JSON
+}
 
 module.exports = merge(common, {
-    mode: 'production'
-});
+  mode: "production",
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "./manifest.json",
+          to: "../",
+          context: "public",
+          force: true,
+          transform(content, path) {
+            return modify(content)
+          },
+        },
+      ],
+    }),
+  ],
+})
